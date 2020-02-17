f09f42cc0a7ca11001d804f45fc7c984b95b8e6a
camilla-design <56583550+camilla-design@users.noreply.github.com>
2020-02-17 13:01:10 +0100

    started

--- js/script.js
+++ js/script.js
@@ -1,5 +1,46 @@
+const rickAndMortyUrl = "https://rickandmortyapi.com/api/character/";
 
-        
+fetch(rickAndMortyUrl)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(json) {
+        handleRickAndMorty(json);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
 
-    
-    
+function handleRickAndMorty(json) {
+
+    const results = json.results;
+    console.dir(results);
+
+    const resultsContainer = document.querySelector(".row.results");
+
+    let html = "";
+    let typeValue;
+
+    for(let i = 0; i < results.lenght; i++) {
+        if(results[i].type) {
+            typeValue = results[i].type;
+        } else {
+            typeValue = "Unknown";
+        }
+    }
+
+   results.forEach(function(result) {
+       html += `<div class="col-sm-6 col-md-4 col-lg-3">                
+                    <div class="card">    
+                    <img class="image" src="${result.image}" alt="Character Name">
+                    <div class="details">
+                    <h4 class="name">${result.name}</h4>
+                    <p>Type: ${result.type}</p>    
+                    <p>Episode count: ${result.episode}</p>                                  
+               <a class="btn btn-primary" href="details.html?id=${result.id}">Details</a>
+           </div>
+       </div>
+   </div>`;
+   });
+   resultsContainer.innerHTML = html;
+}
