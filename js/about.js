35470e4ebb07fc7591f621cb1c86b8bba9a0c880
camilla-design <56583550+camilla-design@users.noreply.github.com>
2020-05-28 15:32:55 +0200

    edited bugs

--- js/about.js
+++ js/about.js
@@ -1,9 +1,8 @@
-const counter = document.querySelector(".container content");
 
 function replaceWords() {
     document.body.innerHTML = document.body.innerHTML.replace(/The/g, "Replaced").replace(/the/g, "replaced");
 }
 
-setTimeout(replaceWords, 4000);
+setTimeout(replaceWords, 3000);
 
 
